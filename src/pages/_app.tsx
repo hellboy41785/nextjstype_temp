9be13e8a4244ba54d4e93b955a3aa38a952724a9
hellboy41785 <unknown41785@gmail.com>
2023-07-05 22:55:29 +0530

    Updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import { useState } from "react";
 import {
   Hydrate,
@@ -7,9 +7,9 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Layout from '@/Layout/Layout';
+import Layout from "@/layout/Layout";
 import Head from "next/head";
-
+import { ThemeProvider } from "@/components/theme-provider";
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
@@ -21,14 +21,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <link
           rel="icon"
           type="text/html"
-          // href="https://img.icons8.com/bubbles/500/null/cancel-2.png"
         />
       </Head>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ThemeProvider>
           <ReactQueryDevtools initialIsOpen={false} />
         </Hydrate>
       </QueryClientProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import MenuToggle from "@/components/theme/MenuToggle";
+
 export default function Home() {
   return (
-    <>
-     <h1>Hello</h1>
-    </>
-  )
+    <div className="flex flex-col w-full min-h-screen gap-4">
+      <div className="flex justify-end p-6">
+        <MenuToggle />
+      </div>
+      <h1 className="p-2 text-4xl font-bold  min-h-max h-[600px] flex justify-center items-center">Hello</h1>
+    </div>
+  );
 }
