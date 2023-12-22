f91e79ddeb5ad889d80cd424ae2c4b213182f9eb
hellboy41785 <unknown41785@gmail.com>
2023-12-22 16:12:18 +0530

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,13 @@ export default function Home() {
                  <div className="flex justify-end p-6">
                              <MenuToggle />
       </div>
-      <h1 className="p-2 text-4xl font-bold  min-h-max h-[600px] flex justify-center items-center">
-        Hello
-      </h1>
+
+
+
+      
+                <h1 className="p-2 text-4xl font-bold  min-h-max h-[600px] flex justify-center items-center">
+                  Hello
+                </h1>
     </div>
   );
 }
